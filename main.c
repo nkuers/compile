2a3065238ab6ed978f8fbabf93b94e0311c2c9fb
nkuers <3287049405@qq.com>
2024-09-17 18:56:14 +0800

    Initial commit

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int i, n, f;
+    i = 2;
+    f = 1;
+    
+    
+    while (i <= n) {
+        f = f * i;
+        i = i + 1;
+    }
+
+   
+    printf("%d\n", f);
+
+    return 0;
+}
